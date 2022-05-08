19a6be0cc7aad69567b8e9d470b1d7286be5e6bd
Akshay <naikwade.akshay1922@gmail.com>
2022-05-09 00:28:24 +0530

    change to sh file

--- jenkins-file1.sh
+++ jenkins-file1.sh
@@ -1,20 +1,14 @@
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                echo 'This is Build Stage.'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'This is Test Stage.'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'This is Deploy Stage.'
-            }
-        }
-    }
-}
+echo "BUILD_NUMBER" :: $BUILD_NUMBER
+echo "BUILD_ID" :: $BUILD_ID
+echo "BUILD_DISPLAY_NAME" :: $BUILD_DISPLAY_NAME
+echo "JOB_NAME" :: $JOB_NAME
+echo "EXECUTOR_NUMBER" :: $EXECUTOR_NUMBER
+echo "NODE_NAME" :: $NODE_NAME
+echo "NODE_LABELS" :: $NODE_LABELS
+echo "WORKSPACE" :: $WORKSPACE
+echo "JENKINS_HOME" :: $JENKINS_HOME
+echo "JENKINS_URL" :: $JENKINS_URL
+echo "BUILD_URL" ::$BUILD_URL
+echo "JOB_URL" :: $JOB_URL
+echo "Below output is all the environment variable in Jenkins"
+printenv
